565780339d3a42a745131754b7d26239439ad81f
Bobby Walters <jubale1328@zoho.com>
2026-05-24 16:52:47 -0700

    Auth protection problem

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup_view, name='signup_view'),
-    path('logout', views.logout, name='logout'),
-    path('profile', views.profile_view, name='profile_view'),
 ]
