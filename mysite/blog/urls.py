157a14c76ab50723d81955b5d0dfc33a0897d566
Bobby Walters <jubale1328@zoho.com>
2026-05-31 17:15:09 -0700

    Logout not working

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup_view, name='signup_view'),
-    path('/profile', views.profile_view, name='profile_view'),
+    path('profile', views.profile_view, name='profile_view'),
 ]
