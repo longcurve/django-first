7807bfcc31b7381c8936e30951d1b336b657e08d
Bobby Walters <jubale1328@zoho.com>
2026-06-03 06:55:15 -0700

    Login not working again

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('registration/', include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
-    path('registration/', include("django.contrib.auth.urls")),
+    
 ]
